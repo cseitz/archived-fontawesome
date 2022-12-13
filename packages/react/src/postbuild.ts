3753d0239488987dae2140d4e61d39d92a86ff2d
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-12-12 21:56:41 -0500

    hmmm another idea

---
+++ packages/react/src/postbuild.ts
@@ -0,0 +1,10 @@
+import { copyFileSync, existsSync, readdirSync, unlinkSync } from 'fs';
+
+
+readdirSync(__dirname + '/../dist').filter(o => o.endsWith('.d.ts'))
+    .filter(o => !(o.startsWith('_index'))).forEach(o => {
+        unlinkSync(__dirname + '/../dist/' + o);
+    })
+
+copyFileSync(__dirname + '/../dist/_index.d.ts', __dirname + '/../dist/index.d.ts');
+unlinkSync(__dirname + '/../dist/_index.d.ts');
