20b2fd8e31201b4bfdc0922cb4b225c249a4a3ac
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-12-12 22:08:41 -0500

    small fix

--- packages/react/src/postbuild.ts
+++ packages/react/src/postbuild.ts
@@ -8,12 +8,3 @@ readdirSync(__dirname + '/../dist').filter(o => o.endsWith('.d.ts'))
 
 copyFileSync(__dirname + '/../dist/_index.d.ts', __dirname + '/../dist/index.d.ts');
 unlinkSync(__dirname + '/../dist/_index.d.ts');
-
-const __define = __dirname + '/../dist/_define.js';
-if (existsSync(__define)) {
-    writeFileSync(__define, readFileSync(__define, 'utf8')
-        .replace(/"\$INSTALLED_ICON_LIBRARIES\$"/, (
-            "'" + JSON.stringify([]) + "'"
-        ))
-    )
-}
