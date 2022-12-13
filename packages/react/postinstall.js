49c89d175ab946d14f63e615f7d858f0f55913f6
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2022-12-12 21:25:08 -0500

    a

--- packages/react/postinstall.js
+++ packages/react/postinstall.js
@@ -1,7 +1,7 @@
 const { readFileSync, writeFileSync, readdirSync, existsSync } = require("fs");
 
 
-const __define = __dirname + '/dist/_define.js';
+const __define = __dirname + '/_define.js';
 if (existsSync(__define)) {
     writeFileSync(__define, readFileSync(__define, 'utf8')
         .replace(/\$INSTALLED_ICON_LIBRARIES\$/, (
